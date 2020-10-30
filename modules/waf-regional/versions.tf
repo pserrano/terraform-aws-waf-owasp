44cb26c3e323ee555763daced2217b98653f3489
Pablo Serrano <info@pabloserrano.net>
2020-10-30 16:46:48 +0100

    feat: Upgrade to terraform 0.13

--- modules/waf-regional/versions.tf
+++ modules/waf-regional/versions.tf
@@ -1,8 +1,11 @@
 
 terraform {
-  required_version = ">= 0.12.28"
+  required_version = ">= 0.13"
 
   required_providers {
-    aws = ">= 2.70.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 2.70.0"
+    }
   }
 }
